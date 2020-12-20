8a7b4272954105b9255142b38f90cb30b4ef95d7
earsnot <sjlund@gmail.com>
2020-12-20 16:16:04 +0100

    removed devel and build from repo

--- build/dynamixel_motor-kinetic/dynamixel_motor/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/catkin_ws/src/dynamixel_motor-kinetic/dynamixel_motor
-# Build directory: /home/ros/catkin_ws/build/dynamixel_motor-kinetic/dynamixel_motor
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
